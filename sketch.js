a06dc1714a738c925f75f301ec2142d9c9790e32
LuizHenriqueDev <94867059+LuizHenriqueDev@users.noreply.github.com>
2021-11-22 17:02:55 -0300

    Add files via upload
    mudar fundo se tocar na tecla

--- sketch.js
+++ sketch.js
@@ -9,15 +9,19 @@ function draw()
   background(30);
   drawSprites();
   if(keyDown("left")){
-    bloco.x = bloco.x-3
+    background("blue")
+    bloco.x = bloco.x-3;
   }
   if(keyDown("right")){
+    background("pink")
     bloco.x = bloco.x+3
   }
   if(keyDown("UP_ARROW")){
+    background("black")
     bloco.y = bloco.y-3
   }
   if(keyDown("down")){
+    background("red")
     bloco.y = bloco.y+3
   }
 
