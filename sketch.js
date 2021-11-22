95000ed0310781625e3671a168f9081f6129bd80
LuizHenriqueDev <94867059+LuizHenriqueDev@users.noreply.github.com>
2021-11-22 16:40:47 -0300

    Add files via upload
    batata é bom

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var bloco
+function setup() {
+  createCanvas(400,400);
+  bloco = createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  if(keyDown("left")){
+    bloco.x = bloco.x-3
+  }
+  if(keyDown("right")){
+    bloco.x = bloco.x+3
+  }
+
+
+
+}
+
+
+
+
