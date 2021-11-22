9e6f22ebac47fc03ac2f1f90843098d1c8ef4322
LuizHenriqueDev <94867059+LuizHenriqueDev@users.noreply.github.com>
2021-11-22 16:49:39 -0300

    Add files via upload
    subir e descer

--- sketch.js
+++ sketch.js
@@ -14,10 +14,12 @@ function draw()
   if(keyDown("right")){
     bloco.x = bloco.x+3
   }
-  if(keyDow("up")){
+  if(keyDown("UP_ARROW")){
     bloco.y = bloco.y-3
   }
-
+  if(keyDown("down")){
+    bloco.y = bloco.y+3
+  }
 
 
 }
